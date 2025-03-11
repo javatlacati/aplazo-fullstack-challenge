9b6de9c0a0d96fc982e303843d6809189fa8c507
Javatlacati <scherzo_16@hotmail.com>
2025-03-11 13:03:35 -0600

    configuración de auto-documentación con SpringDocs

--- back/src/main/java/mx/aplazo/config/SpringDocConfig.java
+++ back/src/main/java/mx/aplazo/config/SpringDocConfig.java
@@ -1,16 +1,30 @@
 package mx.aplazo.config;
 
-import org.springdoc.core.models.GroupedOpenApi;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SpringDocConfig {
+//    @Bean
+//    public GroupedOpenApi customOpenApi() {
+//        return GroupedOpenApi.builder()
+//               .group("aplazo")
+//               .pathsToMatch("/v1/**")
+//               .build();
+//    }
+
     @Bean
-    public GroupedOpenApi customOpenApi() {
-        return GroupedOpenApi.builder()
-               .group("aplazo")
-               .pathsToMatch("/v1/**")
-               .build();
+    public OpenAPI aplazoOpenApi() {
+    return new OpenAPI()
+        .info(
+            new Info()
+                .title("Aplazo BNPL API")
+                .version("0.0.1")
+                .description(
+                    "### Minimal BNPL API\n"
+                        + "\n"
+                        + "    Allows for customer and loan creation."));
     }
 }
