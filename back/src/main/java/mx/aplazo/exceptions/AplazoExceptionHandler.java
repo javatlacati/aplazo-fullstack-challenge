16f264bf9d675c86b96ad83a1fc276424c629fbd
Javatlacati <scherzo_16@hotmail.com>
2025-03-11 20:09:00 -0600

    preventing swagger to detect controller advice as an endpoint

--- back/src/main/java/mx/aplazo/exceptions/AplazoExceptionHandler.java
+++ back/src/main/java/mx/aplazo/exceptions/AplazoExceptionHandler.java
@@ -1,5 +1,6 @@
 package mx.aplazo.exceptions;
 
+import io.swagger.v3.oas.annotations.Hidden;
 import mx.aplazo.domain.ErrorResponse;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
@@ -9,12 +10,13 @@ import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.MissingPathVariableException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @Order(Ordered.HIGHEST_PRECEDENCE)
-@ControllerAdvice
+@RestControllerAdvice
+@Hidden
 public class AplazoExceptionHandler extends ResponseEntityExceptionHandler {
 
   @Override
